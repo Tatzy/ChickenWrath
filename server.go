ae6972cfd5e68285f59264908d7778b7addee171
Tatzy <phosphorus@dr.com>
2019-05-16 15:23:08 -0400

    Update server.go

--- server.go
+++ server.go
@@ -1,75 +1,107 @@
 package main
 
 import (
-	"bufio"
+    "strings"
+    "net/http"
+	"log"
 	"fmt"
-	"math/rand"
-	"net"
-	"os"
-	"strconv"
-	"strings"
-	"time"
-	"bytes"
-
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	//"github.com/tomasen/realip"
 )
 
-const MIN = 1
-const MAX = 100
-
 type player struct {
-	xl, yl, xg, yg int
+	ip string `json:"Ip"`
+	Xl int `json:"Xl"`
+	Yl int `json:"Yl"`
+	Xg int `json:"Xg"`
+	Yg int `json:"Yg"`
+}
+type ipList struct {
+	p1 player
+	p2 player
+	p3 player
+	p4 player
 }
 
-func random() int {
-	return rand.Intn(MAX-MIN) + MIN
+
+
+type msg struct {
+	Id string
+	Content string
 }
 
-func handleConnection(c net.Conn) {
-	var player1 player
-	player1.xl, player1.yl, player1.xg, player1.yg = 0, 0, 0, 0
-	c.Write()
-	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
-	for {
-		netData, err := bufio.NewReader(c).ReadString('\n')
-		
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		
-		temp := strings.TrimSpace(string(netData))
-		if temp == "STOP" {
-			break
-		}
-
-		result := strconv.Itoa(20) + "\n"
-		c.Write([]byte(string(result)))
+var ips ipList
+var players int
+var newmsg player
+func serve(w http.ResponseWriter, req *http.Request) {
+	players += 1
+	inip := strings.Split(req.RemoteAddr, ":")[0]
+	inip = strings.Split(inip, "\n")[0]
+	ips.p1.ip = inip
+	//if ip == "127.0.0.1" {fmt.Println(ips.ip1)}
+	switch players {
+	case 1:
+		ips.p1.ip = inip
+	case 2:
+		ips.p2.ip = inip
+	case 3:
+		ips.p3.ip = inip
+	case 4: 
+		ips.p4.ip = inip
 	}
-	c.Close()
+	
 }
+
+func setup(w http.ResponseWriter, req *http.Request){
+	inip := strings.Split(req.RemoteAddr, ":")[0]
+	inip = strings.Split(inip, "\n")[0]
+	
+	switch {
+	case inip == ips.p1.ip:
+		newmsg := player{ips.p1.ip, 0,players,0,0}
+		f, err := json.Marshal(&newmsg)
+		if err != nil {
+			fmt.Println("nup")
+		}
+		w.Write(f)
+	}
+	fmt.Println("we're here")
+	
+	
+}
+type test_struct struct {
+    Test string
+}
+
+func thing(w http.ResponseWriter, req *http.Request){
+	body, err := ioutil.ReadAll(req.Body)
+    if err != nil {
+        panic(err)
+    }
+    //log.Println(string(body))
+    //var t test_struct
+    err = json.Unmarshal(body, &newmsg)
+    if err != nil {
+        panic(err)
+	}
+	fmt.Println(newmsg.Xl)
+	fmt.Println(newmsg.Yl)
+}
+
 
 func main() {
-	arguments := os.Args
-	if len(arguments) == 1 {
-		fmt.Println("Please provide a port number!")
-		return
+	players = -1
+	r := mux.NewRouter()
+	r.HandleFunc("/", serve)
+	r.HandleFunc("/setup", setup)
+	r.HandleFunc("/coords", thing)
+	http.Handle("/", r)
+	
+	err := http.ListenAndServe(":8000", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
 	}
-
-	PORT := ":" + arguments[1]
-	l, err := net.Listen("tcp4", PORT)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer l.Close()
-	rand.Seed(time.Now().Unix())
-
-	for {
-		c, err := l.Accept()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		go handleConnection(c)
-	}
+	
 }
