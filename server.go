4645857cea8bdd9ebd3bc8177f5f602114759f5e
Tatzy <phosphorus@dr.com>
2019-05-15 21:23:00 -0400

    Update server.go

--- server.go
+++ server.go
@@ -1,65 +1,75 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "math/rand"
-    "net"
-    "os"
-    "strconv"
-    "strings"
-    "time"
+	"bufio"
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+	"bytes"
+
 )
 
 const MIN = 1
 const MAX = 100
 
+type player struct {
+	xl, yl, xg, yg int
+}
+
 func random() int {
-    return rand.Intn(MAX-MIN) + MIN
+	return rand.Intn(MAX-MIN) + MIN
 }
 
 func handleConnection(c net.Conn) {
-    fmt.Printf("Serving %s\n", c.RemoteAddr().String())
-    for {
-        netData, err := bufio.NewReader(c).ReadString('\n')
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
+	var player1 player
+	player1.xl, player1.yl, player1.xg, player1.yg = 0, 0, 0, 0
+	c.Write()
+	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
+	for {
+		netData, err := bufio.NewReader(c).ReadString('\n')
+		
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		
+		temp := strings.TrimSpace(string(netData))
+		if temp == "STOP" {
+			break
+		}
 
-        temp := strings.TrimSpace(string(netData))
-        if temp == "STOP" {
-            break
-        }
-
-        result := strconv.Itoa(random()) + "\n"
-        c.Write([]byte(string(result)))
-    }
-    c.Close()
+		result := strconv.Itoa(20) + "\n"
+		c.Write([]byte(string(result)))
+	}
+	c.Close()
 }
 
 func main() {
-    arguments := os.Args
-    if len(arguments) == 1 {
-        fmt.Println("Please provide a port number!")
-        return
-    }
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide a port number!")
+		return
+	}
 
-    PORT := ":" + arguments[1]
-    l, err := net.Listen("tcp4", PORT)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer l.Close()
-    rand.Seed(time.Now().Unix())
+	PORT := ":" + arguments[1]
+	l, err := net.Listen("tcp4", PORT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer l.Close()
+	rand.Seed(time.Now().Unix())
 
-    for {
-        c, err := l.Accept()
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
-        go handleConnection(c)
-    }
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go handleConnection(c)
+	}
 }
